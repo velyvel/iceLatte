281308f112b2a0f8f7389d361aec8c66e2c5d617
ys <ys@naver.com>
2022-12-16 16:50:33 +0900

    공원 마커 표시

--- copyCatLatte/src/pages/ParkInfo.js
+++ copyCatLatte/src/pages/ParkInfo.js
@@ -1,9 +1,9 @@
 import { Helmet } from 'react-helmet-async';
-import React, { useEffect } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
 // @mui
 import { Container } from '@mui/material';
-// mock
-import ParkInfoData from '../_mock/parkInfoData';
+import ParkList from '../_mock/ParkList';
 
 
 
@@ -11,77 +11,109 @@ import ParkInfoData from '../_mock/parkInfoData';
 
 const { kakao } = window;
 export default function ParkInfo() {
-    useEffect(() => {
-        mapscript();
-    }, []);
+  const [parks, setParks] = useState(null);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [visible, setVisible] = useState(false);
 
-    const mapscript = () => {
+    useEffect(() => {
+      const loadData = async () => {
+        const url = 'http://openAPI.seoul.go.kr:8088/4d514f786b79757337386270766a6d/json/SearchParkInfoService/1/131/';
+        const response = await axios.get(url);
+        const dataCluster = JSON.stringify(response.data.SearchParkInfoService.row);
+        const dataList = JSON.parse(dataCluster);
+        setParks(dataList);
+    }
+    loadData();
+    mapscript();
+      }, []);
+    
+      const mapscript = () => {
         const container = document.getElementById("myMap");
         const options = {
-            center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
-            level: 5,
+          center: new kakao.maps.LatLng(37.5735, 126.9758875),
+          level: 7,
         };
-
+        // eslint-disable-next-line
         const map = new kakao.maps.Map(container, options);
 
-        // 마커 표시할 데이터 불러오기
-        ParkInfoData.forEach((el) => {
-            const marker = new kakao.maps.Marker({
-                map,
-                position: new kakao.maps.LatLng(el.lat, el.lng),
-            });
+        };
+        
+        // 클릭하면 마커 표시할 데이터 불러오기
+        const clickData = (parks) => {
+          const container = document.getElementById("myMap");
+          const options = {
+            center: new kakao.maps.LatLng(37.5735, 126.9758875),
+            level: 7,
+          };
+      
+          const map = new kakao.maps.Map(container, options);
 
-            const infowindow = new kakao.maps.InfoWindow({
-                content: el.title
-            });
-
-            //   마우스 올리면 이름 표시되게 하기
+            parks.forEach((park) => {
+              const marker = new kakao.maps.Marker({
+                  map,
+              position: new kakao.maps.LatLng(park.LATITUDE, park.LONGITUDE),
+              text:park.P_PARK
+              });
+              marker.id=park.P_IDX
+              const infowindow = new kakao.maps.InfoWindow({
+                content: park.P_PARK
+              });
+              
+              // 마우스 올리면 이름 표시하기
             kakao.maps.event.addListener(
-                marker,
-                "mouseover",
-                makeOverListener(map, marker, infowindow)
+              marker,
+              "mouseover",
+              makeOverListener(map, marker, infowindow)
             );
+            // 마우스 치우면 이름 표시 지우기
             kakao.maps.event.addListener(
-                marker,
-                "mouseout",
-                makeOutListener(infowindow)
+              marker,
+              "mouseout",
+              marker.id===parks[marker.id].P_IDX ? console.log('') : makeOutListener(infowindow)
             );
-        });
-
-        function makeOverListener(map, marker, infowindow) {
-            return function () {
-                infowindow.open(map, marker);
-            };
-        }
-        function makeOutListener(infowindow) {
-            return function () {
+            
+              // 마커 클릭하면 해당 장소 정보 저장
+              // eslint-disable-next-line
+              kakao.maps.event.addListener(marker, 'click', (e) => {
                 infowindow.close();
-            };
-        }
+                setVisible(!visible)
+                setSelectedPlace(park);
+                infowindow.open(marker);
+              });
+              }) 
+              
+              // 마우스 올리고 내릴때 정보창 켰다껐다하기
+              function makeOverListener(map, marker, infowindow) {
+                return function clickOpen() {
+                  infowindow.open(map, marker);
+                };
+              }
+              function makeOutListener(infowindow) {
+                return function clickClose() {
+                  infowindow.close();
+                };
+              }
 
-    };
+      };
+    
     return (
         <>
             <Helmet>
                 <title>공원 조회 서비스</title>
             </Helmet>
             <Container>
-                <h4>공원 조회</h4>
+            <button type="button" onClick={clickData.bind(null,parks)}>공원 위치 보기</button>
                 <div id='myMap'
-                     style={{
-                         width: 1000,
-                         height: 500
-                     }} />
+                    style={{
+                    width: 1000,
+                    height: 500
+                }} />
                 <br/><br/>
             </Container>
             <Container>
-                <h4>공원 리스트</h4>
-                <br/><br/>
-                { /* <ParkInfoApi /> */ }
+              {visible ? <ParkList park={selectedPlace} /> : ''}
             </Container>
         </>
     );
 }
-
-
 
