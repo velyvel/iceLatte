00189946b1e4daf0d27b2783ca192d62aaa399bb
kiju <kiju2912@naver.com>
2022-12-23 14:39:16 +0900

    버튼 수정

--- copyCatLatte/react-project/copyCatLatte/src/pages/UserPage.js
+++ copyCatLatte/react-project/copyCatLatte/src/pages/UserPage.js
@@ -39,8 +39,10 @@ export default function UserPage() {
         </Stack>
         
         <div style={{padding: 20}}>
-            [<Link to="PetRecommendation/RecommendationDog">강아지 종 조회</Link>]
-            [<Link to="PetRecommendation/RecommendationSearch">고양이 종 조회</Link>]
+        <button type={"button"} className="btn btn-primary"><Link to="PetRecommendation/RecommendationDog" style={{textDecoration:'none', color:'white'}}>강아지 종 조회</Link></button>&nbsp;&nbsp;
+        <button type={"button"} className="btn btn-primary"><Link to="PetRecommendation/RecommendationSearch" style={{textDecoration:'none', color:'white'}}>고양이 종 조회</Link></button>&nbsp;&nbsp;
+
+          
             <hr/>
         </div>
 
