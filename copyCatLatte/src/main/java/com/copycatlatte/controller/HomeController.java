bf529aea843a8a1651742ccd8652f86ba12d0b00
hanjihae <wlgo32@naver.com>
2022-12-27 12:23:07 +0900

    hospital 8080

--- copyCatLatte/src/main/java/com/copycatlatte/controller/HomeController.java
+++ copyCatLatte/src/main/java/com/copycatlatte/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping(path = { "/", "/dashboard/app", "/dashboard/kakaomap", "/dashboard/parkinfo" })
+	@GetMapping(path = { "/", "/dashboard/app", "/dashboard/kakaomap", "/dashboard/parkinfo", "/dashboard/hospital" })
 	public String home() {
 		
 		return "home";
